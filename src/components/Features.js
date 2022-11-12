eb6f8ebe67ee7c19a3a3a2e3a18c5c13f606f776
preshpi <egwuenuprecious7@gamil.com>
2022-11-12 09:44:44 +0100

    benefit feat fix

--- src/components/Features.js
+++ src/components/Features.js
@@ -56,14 +56,14 @@ function Features() {
 
             <div className="px-6 py-4 mb-3 mt-3 rounded-lg shadow-[20px] h-52 lg:w-[300px] m-10 md:w-[320px] w-[250px] flex items-center justify-center lg:hover:w-[420px] dark:bg-[#66CC7B] bg-gray-200 hover:text-white transition-color duration-200">
             <div className="grid place-items-center">
-              <MdBusiness className="text-7xl text-center flex items-center justify-center border-2  border-green-700 rounded-full px-3 py-3 hover:bg-[#75D95B] dark:bg-[#75D95B] hover:border-none transition-color duration-200 text-white"/>
+              <MdBusiness className="text-7xl text-center flex items-center justify-center border-2  border-green-700 rounded-full px-3 py-3 bg-[#75D95B] dark:bg-[#75D95B] hover:border-none transition-color duration-200 text-white"/>
               <h1 className="text-black items-center text-2xl text-center mt-3">Business suite</h1>
             </div>
             </div>
 
             <div className="px-6 py-4 mb-3 mt-3 rounded-lg shadow-[20px] h-52 lg:w-[300px] m-10 md:w-[320px] w-[250px] flex items-center justify-center lg:hover:w-[420px] dark:bg-[#66CC7B] bg-gray-200 hover:text-white transition-color duration-200">
             <div className="grid place-items-center">
-              <FcDocument className="text-7xl text-center flex items-center justify-center border-2  border-green-700 rounded-full px-3 py-3 hover:bg-[#75D95B] dark:bg-[#75D95B] hover:border-none transition-color duration-200 text-white"/>
+              <FcDocument className="text-7xl text-center flex items-center justify-center border-2  border-green-700 rounded-full px-3 py-3 bg-[#75D95B] dark:bg-[#75D95B] hover:border-none transition-color duration-200 text-white"/>
               <h1 className="text-black items-center text-2xl text-center mt-3">Document Blacklisting</h1>
             </div>
             </div>
